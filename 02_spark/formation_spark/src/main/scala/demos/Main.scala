4443dcb36668ae8e67d99ce33230c8bd0f01698f
Christophe-Utopios <christophe.r@utopios.solutions>
2025-12-15 13:43:18 +0100

    demo scala

---
+++ 02_spark/formation_spark/src/main/scala/demos/Main.scala
@@ -0,0 +1,10 @@
+package demos
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+object Main {
+  def main(args: Array[String]): Unit = {
+    //
+  }
+}
+
