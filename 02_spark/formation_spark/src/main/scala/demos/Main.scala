673dccc8a2bfc5bf598d5e9c10ac44d6667c8f18
Christophe-Utopios <christophe.r@utopios.solutions>
2025-12-15 13:50:51 +0100

    demo scala

--- 02_spark/formation_spark/src/main/scala/demos/Main.scala
+++ 02_spark/formation_spark/src/main/scala/demos/Main.scala
@@ -4,7 +4,17 @@ package demos
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 object Main {
   def main(args: Array[String]): Unit = {
-    //
+    // 1. Déclaration des variables
+    val immutable = "ne change pas"
+    var mutable = "peut changer"
+
+    // 2. Collections
+    var liste = List(1,2,3,4,5,6)
+    var tableau = Array("a", "b", "c")
+    var map = Map("prénom" -> "Toto", "age" -> 18)
+
+    // 3. Fonctions anonymes (lambdas)
+    var addition = (a: Int, b : Int) => a + b
   }
 }
 
